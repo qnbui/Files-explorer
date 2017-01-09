20a844b25cf0671ebf0b480b4c1188c778881c09
Unknown <quangnhanbui@gmail.com>
2017-01-09 17:02:30 +0100

    find files
    find files

--- index.php
+++ index.php
@@ -3,8 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title>files explorer</title>
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
+    <?php
+    $scan = scandir('/home/quangb/');
 
+  foreach($scan as $file)
+  {
+      if (is_dir("/home/quangb/$file"))
+      {
+          echo $file.'<br>';
+      }
+  }
+    ?>
   </body>
 </html>
