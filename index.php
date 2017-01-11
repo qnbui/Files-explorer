142dcc864e380c2362a25b4eaa1b0af32c6470ee
Unknown <quangnhanbui@gmail.com>
2017-01-11 14:10:42 +0100

    Beta 1

--- index.php
+++ index.php
@@ -7,45 +7,44 @@
   </head>
   <body>
     <?php
-  $adresse = "/home/quangb/";
+      $adresse = "/home/quangb/";
   
+          if (isset($_GET['dossier'])) {
 
-  if (isset($_GET['dossier'])) {
-    $adresse = $adresse.$_GET['dossier'];
-   }
+            $adresse = $adresse.$_GET['dossier'];
+           }
 
-  $dirs = scandir($adresse);
-  foreach ($dirs as $folder) {
+           $dirs = scandir($adresse);
+           foreach ($dirs as $folder) {
 
-   if (!is_dir("/home/quangb/$folder")) {
+            if (!is_dir("/home/quangb/$folder")) {
 
-    if ($folder == ".."){
+              if (isset($_GET['dossier'])) {
 
-     echo "<img src='images/folder.png'><a href='index.php?dossier='".$_GET['dossier'].">$folder</a><br>";
+               echo "<a href='index.php?dossier='".$_GET['dossier']."$folder/><img id='fich' src='images/file.png'>$folder</a><br>";
 
-    }
-    else {
+              }
+              else {
 
-     if (isset($_GET['dossier'])) {
+               echo "<a href='index.php?dossier='".$_GET['dossier']."'$folder/><img id='fich' src='images/file.png'>$folder</a><br>";
 
-      echo "<img src='images/file.png'><a href='index.php?dossier='".$_GET['dossier']."$folder/>$folder</a><br>";
+              }
+            }
 
-     }
+            else {
+              if
+                if ($folder == ".."){
 
-     else {
+              echo "<a href='index.php?dossier=".dirname($_GET['dossier'])."'><img id='doss' src='images/file.png'>$folder</a><br>";
 
-      echo "<img src='images/file.png'><a href='index.php?dossier='".$_GET['dossier']."'$folder/>$folder</a><br>";
+             }
+             else {
+             echo "<a href='index.php?dossier=$folder/'><img id='doss' src='images/folder.png'>$folder</a><br>";
+            }
+             }
 
-     }
+           }
 
-    }
-   }
-
-   else {
-    echo "<img src='images/folder.png'><a href='index.php?dossier=$folder/'>$folder</a><br>";
-   }
-
-  }
-                  ?>
+      ?>
   </body>
 </html>
